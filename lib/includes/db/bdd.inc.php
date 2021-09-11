7442c9528b9b101ad9f85fccc968cb462ce49aef
ExoTiiKzzz <arthur.lecompte14@gmail.com>
2021-09-11 20:50:59 +0200

    pas grand chose

---
+++ lib/includes/db/bdd.inc.php
@@ -0,0 +1,7 @@
+<?php
+  $server = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "uvlight";
+  $conn = new PDO("mysql:host=$server;dbname=$dbname","$username","$password");
+?>
