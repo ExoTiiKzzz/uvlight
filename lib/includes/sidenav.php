32b9fc153d04e5e14b5784f446647963884b796b
erazor <pierregi31.12@gmail.com>
2021-10-18 13:44:49 +0200

    paths

--- lib/includes/sidenav.php
+++ lib/includes/sidenav.php
@@ -4,11 +4,13 @@ function sidenav($path){
         <div class="sidenav">
             <i class="fas fa-chevron-left"></i>
             <ul>
-                <li><a href="<?php echo $path ?>#">Test</a></li>
-                <li><a href="<?php echo $path ?>#">Test</a></li>
-                <li><a href="<?php echo $path ?>#">Test</a></li>
-                <li><a href="<?php echo $path ?>#">Test</a></li>
-                <li><a href="<?php echo $path ?>#">Test</a></li>
+                <li><a href="<?php echo $path ?>tables/article/index.php">Article</a></li>
+                <li><a href="<?php echo $path ?>tables/casier/index.php">Casier</a></li>
+                <li><a href="<?php echo $path ?>tables/categorie/index.php">Categorie</a></li>
+                <li><a href="<?php echo $path ?>tables/commune/index.php">Commune</a></li>
+                <li><a href="<?php echo $path ?>tables/departement/index.php">Departement</a></li>
+                <li><a href="<?php echo $path ?>tables/produit/index.php">Produit</a></li>
+                <li><a href="<?php echo $path ?>tables/region/index.php">Région</a></li>
             </ul>
         </div>
     <?php
