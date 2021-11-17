77f807e0c9e28c580c866ffab232f96fd0dc6136
ExoTiiKzzz <arthur.lecompte14@gmail.com>
2021-11-17 21:56:47 +0100

    request optimization

--- tables/article/ajaxfile.php
+++ tables/article/ajaxfile.php
@@ -21,75 +21,54 @@ $searchArray = array();
 ## Search 
 $searchQuery = " ";
 if($searchValue != ''){
-   $searchQuery = " AND (article.art_nom LIKE :art_nom OR article.art_commentaire LIKE :art_commentaire OR article.fk_cat_ID IN (SELECT cat_ID FROM ".DB_TABLE_CATEGORIE." WHERE cat_nom LIKE :categorie) 
-   OR article.fk_cas_ID IN (SELECT cas_ID FROM ".DB_TABLE_CASIER." WHERE cas_lib LIKE :casier))";
+   $searchQuery = " AND (stockfinal.art_nom LIKE :art_nom OR stockfinal.art_commentaire LIKE :art_commentaire OR stockfinal.fk_cat_ID IN (SELECT cat_ID FROM ".DB_TABLE_CATEGORIE." WHERE cat_nom LIKE :categorie) 
+   OR stockfinal.fk_cas_ID IN (SELECT cas_ID FROM ".DB_TABLE_CASIER." WHERE cas_lib LIKE :casier))";
    $searchArray = array( 
-        'art_nom'=>"%$searchValue%",
-        'art_commentaire'=>"%$searchValue%",
-        'categorie'=>"%$searchValue%",
-        'casier'=>"%$searchValue%"
+        ':art_nom'=>"%$searchValue%",
+        ':art_commentaire'=>"%$searchValue%",
+        ':categorie'=>"%$searchValue%",
+        ':casier'=>"%$searchValue%"
    );
 }
 
 ## Total number of records without filtering
-$stmt = $conn->prepare("SELECT stockfinal.art_ID, stockfinal.art_commentaire, stockfinal.art_nom, stockfinal.fk_cat_ID, stockfinal.fk_cas_ID, stockfinal.stock 
-FROM 
-(SELECT (achetee.quantite_achetee - vendue.quantite_vendue) as stock, achetee.quantite_achetee, vendue.quantite_vendue, art_ID, art_commentaire, art_nom, fk_cat_ID, fk_cas_ID
-   FROM article, 
-   (SELECT SUM(Lign_quantite) AS quantite_vendue, fk_art_ID FROM lignes_commande WHERE Lign_is_vente = 1 GROUP BY fk_art_ID ) AS vendue, 
-   (SELECT SUM(Lign_quantite) AS quantite_achetee, fk_art_ID FROM lignes_commande WHERE Lign_is_vente = 0 GROUP BY fk_art_ID ) AS achetee
-         
-   WHERE vendue.fk_art_ID = article.art_ID AND achetee.fk_art_ID = article.art_ID GROUP BY art_ID) as stockfinal,
-   article
-   
-   
-
-WHERE art_is_visible = 1 AND stockfinal.art_ID != 0 GROUP BY stockfinal.art_ID");
+$stmt = $conn->prepare("SELECT COUNT(*) as allcount
+FROM article
+WHERE art_is_visible = 1 AND art_ID != 0");
 $stmt->execute();
-$records = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$totalRecords = count($records);
+$records = $stmt->fetch(PDO::FETCH_ASSOC);
+$totalRecords = $records["allcount"];
 
 ## Total number of records with filtering
-$stmt = $conn->prepare("SELECT COUNT(*) AS allcount, stockfinal.art_ID, stockfinal.art_commentaire, stockfinal.art_nom, stockfinal.fk_cat_ID, stockfinal.fk_cas_ID, stockfinal.stock 
-FROM 
-(SELECT (achetee.quantite_achetee - vendue.quantite_vendue) as stock, achetee.quantite_achetee, vendue.quantite_vendue, art_ID, art_commentaire, art_nom, fk_cat_ID, fk_cas_ID
-   FROM article, 
-   (SELECT SUM(Lign_quantite) AS quantite_vendue, fk_art_ID FROM lignes_commande WHERE Lign_is_vente = 1 GROUP BY fk_art_ID ) AS vendue, 
-   (SELECT SUM(Lign_quantite) AS quantite_achetee, fk_art_ID FROM lignes_commande WHERE Lign_is_vente = 0 GROUP BY fk_art_ID ) AS achetee
-         
-   WHERE vendue.fk_art_ID = article.art_ID AND achetee.fk_art_ID = article.art_ID GROUP BY art_ID) as stockfinal,
-   article
-   
-   
-
-WHERE art_is_visible = 1 AND stockfinal.art_ID != 0 ".$searchQuery." GROUP BY stockfinal.art_ID");
+$stmt = $conn->prepare("SELECT COUNT(*) AS allcount 
+   FROM article
+WHERE art_is_visible = 1 AND art_ID != 0 ".$searchQuery);
 $stmt->execute($searchArray);
-$records = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$totalRecordwithFilter = count($records);
+$records = $stmt->fetch(PDO::FETCH_ASSOC);
+$totalRecordwithFilter = $records["allcount"];
 
 ## Fetch records
 $stmt = $conn->prepare("SELECT stockfinal.art_ID, stockfinal.art_commentaire, stockfinal.art_nom, stockfinal.fk_cat_ID, stockfinal.fk_cas_ID, stockfinal.stock 
 FROM 
 (SELECT (achetee.quantite_achetee - vendue.quantite_vendue) as stock, achetee.quantite_achetee, vendue.quantite_vendue, art_ID, art_commentaire, art_nom, fk_cat_ID, fk_cas_ID
    FROM article, 
-   (SELECT SUM(Lign_quantite) AS quantite_vendue, fk_art_ID FROM lignes_commande WHERE Lign_is_vente = 1 GROUP BY fk_art_ID ) AS vendue, 
-   (SELECT SUM(Lign_quantite) AS quantite_achetee, fk_art_ID FROM lignes_commande WHERE Lign_is_vente = 0 GROUP BY fk_art_ID ) AS achetee
+   (SELECT SUM(Lign_quantite) AS quantite_vendue, fk_art_ID FROM lignes_commande WHERE Lign_is_vente = 1 GROUP BY fk_art_ID) AS vendue, 
+   (SELECT SUM(Lign_quantite) AS quantite_achetee, fk_art_ID FROM lignes_commande WHERE Lign_is_vente = 0 GROUP BY fk_art_ID) AS achetee
          
-   WHERE art_is_visible = 1 AND  vendue.fk_art_ID = article.art_ID AND achetee.fk_art_ID = article.art_ID GROUP BY art_ID) as stockfinal,
+   WHERE art_is_visible = 1 AND  vendue.fk_art_ID = article.art_ID AND achetee.fk_art_ID = article.art_ID GROUP BY art_ID LIMIT :limit, :offset) as stockfinal,
    article
    
    
 
 WHERE stockfinal.art_ID != 0 ".$searchQuery."
 GROUP BY stockfinal.art_ID
-ORDER BY ".$columnName."
-LIMIT :limit,:offset");
+ORDER BY ".$columnName);
 
 // Bind values
 foreach($searchArray as $key=>$search){
    $stmt->bindValue(':'.$key, $search,PDO::PARAM_STR);
 }
-
+//
 $stmt->bindValue(':limit', (int)$row, PDO::PARAM_INT);
 $stmt->bindValue(':offset', (int)$rowperpage, PDO::PARAM_INT);
 $stmt->execute();
